17ae38e05f3260040437da55f87e231fe1d20dcb
Beets <info@beets3d.com>
2025-03-02 19:30:50 +0800

    change motor PWM and Motor B direction

--- main.py
+++ main.py
@@ -1,50 +1,30 @@
 def spinMotorB(dir2: number, pwm: number):
+    # Control both direction pins (IN1 and IN2)
     if dir2 == FORWARD:
+        pins.digital_write_pin(DigitalPin.P2, 1)  # IN1 High
+        pins.digital_write_pin(DigitalPin.P3, 0)  # IN2 Low
+        pins.analog_write_pin(AnalogPin.P5, pwm)  # Enable PWM (50-1023)
+    elif dir2 == BACKWARD:
+        pins.digital_write_pin(DigitalPin.P2, 0)  # IN1 Low
+        pins.digital_write_pin(DigitalPin.P3, 1)  # IN2 High
+        pins.analog_write_pin(AnalogPin.P5, pwm)  # Enable PWM
+    elif dir2 == OFF:
         pins.digital_write_pin(DigitalPin.P2, 0)
-        pins.analog_write_pin(AnalogPin.P8, pwm)
-    if dir2 == BACKWARD:
-        pins.analog_write_pin(AnalogPin.P2, pwm)
-        pins.digital_write_pin(DigitalPin.P8, 0)
-    if dir2 == OFF:
-        pins.digital_write_pin(DigitalPin.P2, 0)
-        pins.digital_write_pin(DigitalPin.P8, 0)
-    if dir2 == BRAKE:
+        pins.digital_write_pin(DigitalPin.P3, 0)
+        pins.analog_write_pin(AnalogPin.P5, 0)
+    elif dir2 == BRAKE:
         pins.digital_write_pin(DigitalPin.P2, 1)
-        pins.digital_write_pin(DigitalPin.P8, 1)
+        pins.digital_write_pin(DigitalPin.P3, 1)
+        pins.analog_write_pin(AnalogPin.P5, 0)
 
-def on_button_pressed_a():
-    spinMotorA(FORWARD, 1023)
-    basic.show_leds("""
-        . . # . .
-        . # . . .
-        # # # # #
-        . # . . .
-        . . # . .
-        """)
-    control.wait_micros(6000)
-    spinMotorA(BRAKE, 0)
-input.on_button_pressed(Button.A, on_button_pressed_a)
-
-def on_button_pressed_b():
-    spinMotorB(FORWARD, 1023)
-    basic.show_leds("""
-        . . # . .
-        . . . # .
-        # # # # #
-        . . . # .
-        . . # . .
-        """)
-    control.wait_micros(6000)
-    spinMotorB(BRAKE, 0)
-input.on_button_pressed(Button.B, on_button_pressed_b)
+# ... (rest of your code remains unchanged with necessary adjustments) ...
 
 def on_received_value(name, value):
-    global radioX, radioY
-    if name == "x":
-        radioX = value
-    else:
-        if name == "y":
-            radioY = value
+    global radioRight, radioLeft
+    if name == "left":
+        radioRight = value
+    elif name == "right":
+        radioLeft = value
 radio.on_received_value(on_received_value)
 
 def spinMotorA(dir3: number, pwm2: number):
@@ -64,27 +44,46 @@ BRAKE = 0
 FORWARD = 0
 BACKWARD = 0
 OFF = 0
-radioX = 0
-radioY = 0
+radioRight = 0
+radioLeft = 0
 basic.show_icon(IconNames.HOUSE)
 radio.set_group(38)
-radioY = 0
-radioX = 0
+radioLeft = 0
+radioRight = 0
 OFF = 0
 BACKWARD = 1
 FORWARD = 2
 BRAKE = 3
-MOVESTOP = 0
-MOVEFORWARD = 1
-MOVEBACKWARD = 2
-MOVELEFT = 3
-MOVERIGHT = 4
-DISTANCE = 50
-pins.digital_write_pin(DigitalPin.P0, 1)
-pins.digital_write_pin(DigitalPin.P1, 1)
-pins.digital_write_pin(DigitalPin.P2, 1)
-pins.digital_write_pin(DigitalPin.P8, 1)
+PWM_SPEED_MED = 500
+PWD_SPEED_HIGH = 1023
+pins.digital_write_pin(DigitalPin.P0, 0)
+pins.digital_write_pin(DigitalPin.P1, 0)
+pins.digital_write_pin(DigitalPin.P2, 0)
+pins.digital_write_pin(DigitalPin.P5, 0)
 
 def on_forever():
-    pass
+    if input.button_is_pressed(Button.A) or radioRight == 1:
+        spinMotorA(BACKWARD, PWM_SPEED_MED)
+        basic.show_leds("""
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            """)
+    else:
+        spinMotorA(BRAKE, 0)
+        basic.show_icon(IconNames.SQUARE)
+    if input.button_is_pressed(Button.B) or radioLeft == 1:
+        spinMotorB(FORWARD, PWM_SPEED_MED)
+        basic.show_leds("""
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            """)
+    else:
+        spinMotorB(BRAKE, 0)
+        basic.show_icon(IconNames.SQUARE)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,21 +1,27 @@
 function spinMotorB (dir2: number, pwm: number) {
+    // Control both direction pins (IN1 and IN2)
     if (dir2 == FORWARD) {
         pins.digitalWritePin(DigitalPin.P2, 0)
-        pins.analogWritePin(AnalogPin.P8, pwm)
+        // IN2 Low
+        pins.analogWritePin(AnalogPin.P5, pwm)
     }
     if (dir2 == BACKWARD) {
+        // IN2 High
         pins.analogWritePin(AnalogPin.P2, pwm)
-        pins.digitalWritePin(DigitalPin.P8, 0)
+        // Enable PWM (50-1023)
+        pins.digitalWritePin(DigitalPin.P5, 0)
     }
     if (dir2 == OFF) {
+        // Enable PWM
         pins.digitalWritePin(DigitalPin.P2, 0)
-        pins.digitalWritePin(DigitalPin.P8, 0)
+        pins.analogWritePin(AnalogPin.P5, 0)
     }
     if (dir2 == BRAKE) {
         pins.digitalWritePin(DigitalPin.P2, 1)
-        pins.digitalWritePin(DigitalPin.P8, 1)
+        pins.digitalWritePin(DigitalPin.P5, 1)
     }
 }
+// ... (rest of your code remains unchanged with necessary adjustments) ...
 radio.onReceivedValue(function (name, value) {
     if (name == "left") {
         radioRight = value
@@ -47,7 +53,6 @@ let BACKWARD = 0
 let OFF = 0
 let radioRight = 0
 let radioLeft = 0
-let MOVESTOP = 0
 basic.showIcon(IconNames.House)
 radio.setGroup(38)
 radioLeft = 0
@@ -56,18 +61,16 @@ OFF = 0
 BACKWARD = 1
 FORWARD = 2
 BRAKE = 3
-let MOVEFORWARD = 1
-let MOVEBACKWARD = 2
-let MOVELEFT = 3
-let MOVERIGHT = 4
-let DISTANCE = 50
-pins.digitalWritePin(DigitalPin.P0, 1)
-pins.digitalWritePin(DigitalPin.P1, 1)
-pins.digitalWritePin(DigitalPin.P2, 1)
-pins.digitalWritePin(DigitalPin.P8, 1)
+let PWM_SPEED_MED = 500
+let PWD_SPEED_HIGH = 1023
+pins.digitalWritePin(DigitalPin.P0, 0)
+pins.digitalWritePin(DigitalPin.P1, 0)
+pins.digitalWritePin(DigitalPin.P2, 0)
+pins.digitalWritePin(DigitalPin.P3, 0)
+pins.digitalWritePin(DigitalPin.P5, 0)
 basic.forever(function () {
-    if (input.buttonIsPressed(Button.A) || radioRight == 1) {
-        spinMotorA(FORWARD, 1023)
+    if (radioRight == 1) {
+        spinMotorA(BACKWARD, PWM_SPEED_MED)
         basic.showLeds(`
             . . # . .
             . # . . .
@@ -76,11 +79,11 @@ basic.forever(function () {
             . . # . .
             `)
     } else {
-        spinMotorA(BRAKE, 0)
+        spinMotorA(BRAKE, PWM_SPEED_MED)
         basic.showIcon(IconNames.Square)
     }
-    if (input.buttonIsPressed(Button.B) || radioLeft == 1) {
-        spinMotorB(FORWARD, 1023)
+    if (radioLeft == 1) {
+        spinMotorB(FORWARD, PWM_SPEED_MED)
         basic.showLeds(`
             . . # . .
             . . . # .
@@ -89,7 +92,7 @@ basic.forever(function () {
             . . # . .
             `)
     } else {
-        spinMotorB(BRAKE, 0)
+        spinMotorB(BRAKE, PWM_SPEED_MED)
         basic.showIcon(IconNames.Square)
     }
 })
